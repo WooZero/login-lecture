c401bfb26b406075560bb950d7e0eec8b603bfaf
Wooyeong Jang <wkddn1005@naver.com>
2023-01-08 15:40:21 +0900

    프런트 단 javascript

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@
     const app = express();
 
     //포트 변수 선언
-    const PORT = 3001;
+    const PORT = 3000;
 
     //라우팅
     const home = require("./src/routes/home");
@@ -13,6 +13,7 @@
     //앱 세팅
     app.set("views", "./src/views");
     app.set("view engine", "ejs");
+    app.use(express.static(`${__dirname}/src/public`));
 
     app.use("/", home); //use -> 미들 웨어를 등록해주는 메서드
 
